5cc0c20b2623207b8a88dda26e528aecbd7809d5
Jim Dixon <jddixon@gmail.com>
2017-07-06 13:01:23 -0700

    restructure for pytest; add MANIFEST.in

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 import re
 from distutils.core import setup
 __version__ = re.search(r"__version__\s*=\s*'(.*)'",
-                        open('xltransport/__init__.py').read()).group(1)
+                        open('src/xltransport/__init__.py').read()).group(1)
 
 # see http://docs.python.org/distutils/setupscript.html
 
@@ -19,7 +19,7 @@ setup(name='xltransport_py',
       # list it here, without the .py extension but SQuoted
       py_modules=[],
       #
-      packages=['xltransport', ],
+      packages=['src/xltransport', ],
       #
       # following could be in scripts/ subdir; SQuote
       scripts=[],
--- src/xltransport/__init__.py
+++ src/xltransport/__init__.py
@@ -2,8 +2,8 @@
 
 """ Transport package for XLattice python library. """
 
-__version__ = '0.0.5'
-__version_date__ = '2017-06-03'
+__version__ = '0.0.6'
+__version_date__ = '2017-07-06'
 
 __all__ = ['__version__', '__version_date__', 'XLTransportError', ]
 
