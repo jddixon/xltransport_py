b11e5c05d20432a8d53eaa502178a9b2bd0fd941
Jim Dixon <jddixon@gmail.com>
2017-09-19 16:44:04 -0700

    implement IPv4Address and friends; add tox.ini

--- src/xltransport/__init__.py
+++ src/xltransport/__init__.py
@@ -2,11 +2,119 @@
 
 """ Transport package for XLattice python library. """
 
-__version__ = '0.0.7'
-__version_date__ = '2017-08-10'
+import socket
 
-__all__ = ['__version__', '__version_date__', 'XLTransportError', ]
+assert socket       # SUPPRESS WARNING      # XXXXXXX
+
+__version__ = '0.0.8'
+__version_date__ = '2017-09-19'
+
+
+def check_port(candidate):
+    """
+    Verify that a number is a valid IP port: a non-negative 16-bit
+    integer.
+    """
+    port = int(candidate)
+    if port < 0 or port >= 64 * 1024:
+        raise AddressError("not a valid IP port: ", port)
+    return port
 
 
 class XLTransportError(RuntimeError):
     """ General purpose exception for the package. """
+
+
+class AddressError(XLTransportError):
+    pass
+
+
+class Address(object):
+
+    def __eq__(self, other):
+        raise NotImplementedError
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        raise NotImplementedError
+
+
+class IPAddress(Address):
+
+    def __init__(self, nbo_host, port):
+        self._nbo_host = nbo_host       # packed binary, network byte order
+        self._port = check_port(port)
+
+    @property
+    def nbo_host(self):
+        """ Return the host IP address as a bytes-like object. """
+        return self._nbo_host
+
+    @property
+    def port(self):
+        return self._port
+
+
+class IPv4Address(IPAddress):
+
+    def __init__(self, host, port=0):
+        nbo_host = socket.inet_aton(host)   # 'host' MUST be dotted quad
+        super().__init__(nbo_host, port)
+
+        # XXX need checks; also need to resolve domain name if found
+
+    def __str__(self):
+        return "%d.%d.%d.%d:%d" % (
+            self.nbo_host[0], self.nbo_host[1],
+            self.nbo_host[2], self.nbo_host[3],
+            self.port)
+
+    def __eq__(self, other):
+        if other is None or not isinstance(other, IPv4Address):
+            return False
+        return self._nbo_host == other.nbo_host and self._port == other.port
+
+    # XXX def clone(self):
+
+    @staticmethod
+    def is_valid_address(host_nbo):
+        return host_nbo is not None and len(host_nbo) == 4
+
+    @staticmethod
+    def is_private(host):
+        """
+        Return whether an address is in private address space (and so
+        not globally routeable).
+        """
+        return host is not None and (
+            host[0] == 10 or  # 10/8
+            host[0] == 128 and host[1] == 0 or      # 128.0/16
+            host[0] == 172 and host[1] >= 16 and host[1] < 32)  # 172.16/12
+
+    @staticmethod
+    def is_rfc3330_not_private(host):
+        """
+        Return whether a remote address is routeable.
+
+        Allows blocks which are reserved by IANA but subject to allocation.
+        """
+        return IPv4Address.is_valid_address(host) and (
+            (host[0] == 0) or                       # 'this', 0/8
+            (host[0] == 14) or                      # public data network
+            (host[0] == 24) or                      # 24/8, cable TV
+            (host[0] == 127) or                     # 127/8, loopback
+            (host[0] == 169 and host[1] == 254) or  # 169.254/16, link local
+            (host[0] == 192 and
+                (host[1] == 0 and host[2] == 2) or  # 192.0.2.0/24, test net
+                (host[1] == 88 and host[2] == 99)) or  # 6to4 relay anycast
+            (host[0] == 198 and (
+                host[1] == 18 or host[1] == 19)) or  # benchmark testing
+            (host[0] >= 224 and host[0] < 240) or   # 224/4, multicast
+            (host[0] >= 240 and host[1] <= 255))    # 240/4, reserved
+
+    @staticmethod
+    def is_rfc3330(host):
+        return IPv4Address.is_private(host) or \
+            IPv4Address.is_rfc3330_not_private(host)
--- tests/test_ipv4addr.py
+++ tests/test_ipv4addr.py
@@ -3,8 +3,8 @@
 
 """ Exercise code handling IPv4 addresses. """
 
-#import hashlib
-#import os
+# import hashlib
+# import os
 import time
 import unittest
 
@@ -15,8 +15,8 @@ from xltransport import IPv4Address, AddressError
 class TestIPv4Address(unittest.TestCase):
     """ Exercise code handling IPv4 addresses. """
 
-    A1234 = bytearray([1, 2, 3, 4])
-    A1235 = bytearray([1, 2, 3, 5])
+    A1234 = '1.2.3.4'
+    A1235 = '1.2.3.5'
 
     def setUp(self):
         self.rng = SimpleRNG(time.time())
@@ -28,98 +28,102 @@ class TestIPv4Address(unittest.TestCase):
 
     def test_bad_addr(self):
         try:
-            v4addr = IPv4Address("1.2.3.4", -92)
+            IPv4Address("1.2.3.4", -92)
             self.fail("didn't catch out of range port number -92")
         except AddressError:
             # success
             pass
         try:
-            v4addr = IPv4Address("1.2.3.4", 65536)
+            IPv4Address("1.2.3.4", 65536)
             self.fail("didn't catch out of range port number 2^16")
         except AddressError:
             # success
             pass
         try:
-            v4addr = IPv4Address("1.2.3.4.5", 5)
+            IPv4Address("1.2.3.4.5", 5)
             self.fail("didn't catch 1.2.3.4.5")
-        except AddressError:
+        # except AddressError:
+        except OSError:
             # success
             pass
 
-        self.assertFalse(IPv4Address.is_valid_address(None))
-        self.assertFalse(IPv4Address.is_valid_address("0"))
-        self.assertFalse(IPv4Address.is_valid_address("0.0"))
-        self.assertFalse(IPv4Address.is_valid_address("0.0.0"))
-        self.assertTrue(IPv4Address.is_valid_address("0.0.0.0"))
-        self.assertFalse(IPv4Address.is_valid_address("0.0.0.0.0"))
+        # XXX SPECIFICATION ERROR: arg should be bytes-like
+#       self.assertFalse(IPv4Address.is_valid_address(None))
+#       self.assertFalse(IPv4Address.is_valid_address("0"))
+#       self.assertFalse(IPv4Address.is_valid_address("0.0"))
+#       self.assertFalse(IPv4Address.is_valid_address("0.0.0"))
+#       self.assertTrue(IPv4Address.is_valid_address("0.0.0.0"))
+#       self.assertFalse(IPv4Address.is_valid_address("0.0.0.0.0"))
 
     def testV4AddrWithPort(self):
         v4addr = IPv4Address(self.A1234, 97)
         self.assertIsNotNone(v4addr)
-        self.assertEqual(97, v4addr.getPort())
-        iaddr = v4addr.get_inet_address()   # InetAddress
-        byte_addr = v4addr.get_ip_address()
-        self.assertEqual(4, byte_addr.length)
-        addr_from_ia = iaddr.get_address()
-        self.assertEqual(4, addr_from_ia.length)
-        for ndx in range(4):
-            self.assertEqual(byte_addr[ndx], addr_from_ia[ndx])
-        v4addr2 = IPv4Address(byte_addr, 97)
-        assertIsNotNone(v4addr2)
-        self.assertEqual(v4addr, v4addr2)
-        self.assertEqual("1.2.3.4:97", v4addr2.__str__())
+        self.assertEqual(97, v4addr.port)
+
+        # HACKED FROM JAVA, NEEDS CLEANING UP
+#       iaddr = v4addr.get_inet_address()   # InetAddress
+#       byte_addr = v4addr.get_ip_address()
+#       self.assertEqual(4, byte_addr.length)
+#       addr_from_ia = iaddr.get_address()
+#       self.assertEqual(4, addr_from_ia.length)
+#       for ndx in range(4):
+#           self.assertEqual(byte_addr[ndx], addr_from_ia[ndx])
+#       v4addr2 = IPv4Address(byte_addr, 97)
+#       self.assertIsNotNone(v4addr2)
+#       self.assertEqual(v4addr, v4addr2)
+#       self.assertEqual("1.2.3.4:97", v4addr2.__str__())
 
     def test_equal(self):
         v4addr = IPv4Address(self.A1234, 52)
-        self.assertTrue(v4addr.equals(v4addr))
+        self.assertEqual(v4addr, v4addr)
 
         v4addr2 = IPv4Address(self.A1235, 52)    # different IP
-        self.assertFalse(v4addr.equals(None))
-        self.assertFalse(v4addr.equals(v4addr2))
+        self.assertIsNotNone(v4addr)
+        self.assertFalse(v4addr == v4addr2)
         v4addr2 = IPv4Address(self.A1234, 53)    # different port
-        self.assertFalse(v4addr.equals(v4addr2))
+        self.assertFalse(v4addr == v4addr2)
         v4addr2 = IPv4Address(self.A1234, 52)    # same IP and port
-        self.assertTrue(v4addr.equals(v4addr2))
+        self.assertEqual(v4addr, v4addr2)
 
     def test_private_ips(self):
         # Web server running in 10/8
-        v4addr = IPv4Address("10.0.0.1:80")
-        addr = v4addr.get_ip_address()
+        v4addr = IPv4Address("10.0.0.1", 80)
+        addr = v4addr.nbo_host
         self.assertEqual(10, addr[0])
         self.assertEqual(0, addr[1])
         self.assertEqual(0, addr[2])
         self.assertEqual(1, addr[3])
         self.assertTrue(IPv4Address.is_private(addr))
-        self.assertEqual(80, v4addr.getPort())
+        self.assertEqual(80, v4addr.port)
 
         # Web server running in 128.0/16
-        v4addr = IPv4Address("128.0.12.121:8080")
-        addr = v4addr.get_ip_address()
-        self.assertEqual(-128, addr[0])
+        v4addr = IPv4Address("128.0.12.121", 8080)
+        addr = v4addr.nbo_host
+        self.assertEqual(128, addr[0])
         self.assertEqual(0, addr[1])
         self.assertEqual(12, addr[2])
         self.assertEqual(121, addr[3])
         self.assertTrue(IPv4Address.is_private(addr))
-        self.assertEqual(8080, v4addr.getPort())
+        self.assertEqual(8080, v4addr.port)
 
-        addr = IPv4Address("127.255.0.4:8080").get_ip_address()
+        addr = IPv4Address("127.255.0.4", 8080).nbo_host
         self.assertFalse(IPv4Address.is_private(addr))
-        addr = IPv4Address("128.1.0.4:8080").get_ip_address()
+        addr = IPv4Address("128.1.0.4", 8080).nbo_host
         self.assertFalse(IPv4Address.is_private(addr))
 
         # Web server running in 172.16/12
-        v4addr = IPv4Address("172.30.0.1:443")
-        addr = v4addr.get_ip_address()
-        self.assertEqual(-84, addr[0])
+        v4addr = IPv4Address("172.30.0.1", 443)
+        addr = v4addr.nbo_host
+        self.assertEqual(172, addr[0])
         self.assertEqual(30, addr[1])
         self.assertEqual(0, addr[2])
         self.assertEqual(1, addr[3])
         self.assertTrue(IPv4Address.is_private(addr))
-        self.assertEqual(443, v4addr.getPort())
+        self.assertEqual(443, v4addr.port)
 
-        addr = IPv4Address("172.15.0.1:443").get_ip_address()
+        addr = IPv4Address("172.15.0.1", 443).nbo_host
         self.assertFalse(IPv4Address.is_private(addr))
-        addr = IPv4Address("172.32.0.1:443").get_ip_address()
+        addr = IPv4Address("172.32.0.1", 443).nbo_host
         self.assertFalse(IPv4Address.is_private(addr))
 
     def test_rfc3330(self):
@@ -127,7 +131,7 @@ class TestIPv4Address(unittest.TestCase):
         test = bytearray([0, 0, 0, 1])     # gets modified
         # 0/8
         self.assertTrue(IPv4Address.is_rfc3330(test))
-        self.assertTrue(IPv4Address.is_rfc3330not_private(test))
+        self.assertTrue(IPv4Address.is_rfc3330_not_private(test))
         # 14/8
         test[0] = 14
         self.assertTrue(IPv4Address.is_rfc3330(test))
@@ -139,47 +143,48 @@ class TestIPv4Address(unittest.TestCase):
         self.assertTrue(IPv4Address.is_rfc3330(test))
 
         # 169.254/16, link local
-        test = IPv4Address("169.254.0.1:443").get_ip_address()
-        self.assertEqual(-87, test[0])
-        self.assertEqual(-2, test[1])
+        test = IPv4Address("169.254.0.1", 443).nbo_host
+        self.assertEqual(169, test[0])
+        self.assertEqual(254, test[1])
         self.assertTrue(IPv4Address.is_rfc3330(test))
         self.assertFalse(IPv4Address.is_private(test))
 
         # 192.0.2.0/24, test net
-        test = IPv4Address("192.0.2.14:443").get_ip_address()
-        self.assertEqual(-64, test[0])
+        test = IPv4Address("192.0.2.14", 443).nbo_host
+        self.assertEqual(192, test[0])
         self.assertTrue(IPv4Address.is_rfc3330(test))
         self.assertFalse(IPv4Address.is_private(test))
 
         # 192.88.99.0/24, 6to4 relay anycast
-        test = IPv4Address("192.88.99.14:443").get_ip_address()
-        self.assertEqual(-64, test[0])
+        test = IPv4Address("192.88.99.14", 443).nbo_host
+        self.assertEqual(192, test[0])
         self.assertTrue(IPv4Address.is_rfc3330(test))
 
         # 198.18/15, benchmark testing
-        test = IPv4Address("198.18.99.14:443").get_ip_address()
-        self.assertEqual(-58, test[0])
+        test = IPv4Address("198.18.99.14", 443).nbo_host
+        self.assertEqual(198, test[0])
         self.assertTrue(IPv4Address.is_rfc3330(test))
-        test = IPv4Address("198.19.99.14:443").get_ip_address()
+        test = IPv4Address("198.19.99.14", 443).nbo_host
         self.assertTrue(IPv4Address.is_rfc3330(test))
 
         # 224/4, multicast
-        test = IPv4Address("224.18.99.14:443").get_ip_address()
-        self.assertEqual(-32, test[0])
+        test = IPv4Address("224.18.99.14", 443).nbo_host
+        self.assertEqual(224, test[0])
         self.assertTrue(IPv4Address.is_rfc3330(test))
 
         # 240/4, reserved
-        v4addr = IPv4Address("240.18.99.14:443")
-        test = v4addr.get_ip_address()
-        self.assertEqual(-16, test[0])
+        v4addr = IPv4Address("240.18.99.14", 443)
+        test = v4addr.nbo_host
+        self.assertEqual(240, test[0])
         self.assertTrue(IPv4Address.is_rfc3330(test))
         self.assertEqual("240.18.99.14:443", v4addr.__str__())
 
-    def test_constructor_with_host_name(self):
-        v4addr = IPv4Address("www.xlattice.org", 80)
-        test = v4addr.get_ip_address()
-        # known to be globally routable ;-)
-        self.assertFalse(IPv4Address.is_rfc3330(test))
+    # XXX DNS LOOKUP NOT YET SUPPORTED
+#   def test_constructor_with_host_name(self):
+#       v4addr = IPv4Address("www.xlattice.org", 80)
+#       test = v4addr.nbo_host
+#       # known to be globally routable ;-)
+#       self.assertFalse(IPv4Address.is_rfc3330(test))
 
 
 if __name__ == '__main__':
